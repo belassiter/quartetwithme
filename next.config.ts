d2d6240be264b99095cbe1f6f21bfa4936800e62
Brian Lassiter <51713030+belassiter@users.noreply.github.com>
2025-09-04 10:55:43 -0700

    tempo slider added

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,22 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  webpack(config, { isServer }) {
+    config.module.rules.push({
+      test: /\.glsl$/,
+      loader: 'raw-loader',
+    });
+
+    
+
+    // Add a rule to handle .html files
+    config.module.rules.push({
+      test: /\.html$/,
+      loader: 'html-loader',
+    });
+
+    return config;
+  },
 };
 
 export default nextConfig;
