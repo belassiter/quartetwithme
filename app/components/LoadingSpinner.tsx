eb1f7b415d1bbba6154c2610017e5194f1c15cab
Brian Lassiter <51713030+belassiter@users.noreply.github.com>
2025-09-16 21:23:21 -0700

    resolved IMG warning

--- app/components/LoadingSpinner.tsx
+++ app/components/LoadingSpinner.tsx
@@ -1,10 +1,11 @@
 import React from 'react';
+import Image from 'next/image';
 
 const LoadingSpinner = () => {
   return (
     <div className="spinner-overlay">
       <div className="spinner flex justify-center items-center">
-        <img src="/assets/saxophone-svgrepo-com.svg" alt="Saxophone" className="w-12 h-12" />
+        <Image src="/assets/saxophone-svgrepo-com.svg" alt="Saxophone" width={48} height={48} />
       </div>
     </div>
   );
