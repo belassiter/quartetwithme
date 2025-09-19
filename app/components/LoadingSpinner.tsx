f1e114f982d8f3cb8b9d32049719b725a35388e4
Brian Lassiter <51713030+belassiter@users.noreply.github.com>
2025-09-18 22:55:21 -0700

    minor updates

--- app/components/LoadingSpinner.tsx
+++ app/components/LoadingSpinner.tsx
@@ -5,7 +5,7 @@ const LoadingSpinner = () => {
   return (
     <div className="spinner-overlay">
       <div className="spinner flex justify-center items-center">
-        <Image src="/assets/saxophone-svgrepo-com.svg" alt="Saxophone" width={48} height={48} />
+        <Image src="/assets/saxophone-svgrepo-com.svg" alt="Saxophone" width={48} height={48} priority />
       </div>
     </div>
   );
